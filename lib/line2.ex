4e30a13f9ce4d7598a024dbf3a404a6926bd6272
Mykola Konyk <mykola@konyk.org>
2015-11-28 18:49:54 -0500

    line2: adding initial line2.

--- lib/line2.ex
+++ lib/line2.ex
@@ -1,6 +1,14 @@
 defmodule Geo2d.Line2 do
 
-  # Ax + By + C = 0
-  @type line2 :: { float, float, float }
+  @spec create() :: Geo2d.line2
+  def create(), do: {0.0, 0.0, 0.0}
+
+  @spec create(Geo2d.point2, Geo2d.point2) :: Geo2d.line2
+  def create(_p1, _p2) do
+    {0.0, 0.0, 0.0}
+  end
+
+  @spec create(float, float, float) :: Geo2d.line2
+  def create(a, b, c), do: {a, b, c}
 
 end
