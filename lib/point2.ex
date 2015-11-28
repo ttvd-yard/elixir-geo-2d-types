45407a7cca40cdfa2214b40d591f3c2210c505c8
Mykola Konyk <mykola@konyk.org>
2015-11-28 18:43:43 -0500

    point2, vector2: refactoring types.

--- lib/point2.ex
+++ lib/point2.ex
@@ -1,64 +1,62 @@
 defmodule Geo2d.Point2 do
 
-  @type point2 :: {float, float}
-
-  @spec create() :: point2
+  @spec create() :: Geo2d.point2
   def create(), do: {0.0, 0.0}
 
-  @spec create(float, float) :: point2
+  @spec create(float, float) :: Geo2d.point2
   def create(x, y), do: {x, y}
 
-  @spec create(point2) :: point2
-  @spec create([float]) :: point2
-  @spec create(float) :: point2
+  @spec create(Geo2d.point2) :: Geo2d.point2
+  @spec create([float]) :: Geo2d.point2
+  @spec create(float) :: Geo2d.point2
   def create({x, y}), do: {x, y}
   def create(a) when is_float(a), do: {a, a}
   def create([]), do: {0.0, 0.0}
   def create([x, y|_]), do: {x, y}
   def create([x|_]), do: {x, x}
 
-  @spec to_list(point2) :: [float]
+  @spec to_list(Geo2d.point2) :: [float]
   def to_list({x, y}), do: [x, y]
 
-  @spec zero() :: point2
+  @spec zero() :: Geo2d.point2
   def zero(), do: create()
 
-  @spec x(point2) :: float
+  @spec x(Geo2d.point2) :: float
   def x({x, _y}), do: x
 
-  @spec y(point2) :: float
+  @spec y(Geo2d.point2) :: float
   def y({_x, y}), do: y
 
-  @spec xx(point2) :: point2
+  @spec xx(Geo2d.point2) :: Geo2d.point2
   def xx({x, _y}), do: {x, x}
 
-  @spec yy(point2) :: point2
+  @spec yy(Geo2d.point2) :: Geo2d.point2
   def yy({_x, y}), do: {y, y}
 
-  @spec xy(point2) :: point2
+  @spec xy(Geo2d.point2) :: Geo2d.point2
   def xy(v), do: v
 
-  @spec yx(point2) :: point2
+  @spec yx(Geo2d.point2) :: Geo2d.point2
   def yx({x, y}), do: {y, x}
 
-  @spec at(point2, integer) :: float
+  @spec at(Geo2d.point2, integer) :: float
   def at({x, _y}, 0), do: x
   def at({_x, y}, 1), do: y
 
-  @spec invert(point2) :: point2
+  @spec invert(Geo2d.point2) :: Geo2d.point2
   def invert({x, y}), do: {-x, -y}
 
-  @spec rotate(point2, float) :: point2
+  @spec rotate(Geo2d.point2, float) :: Geo2d.point2
   def rotate({x, y}, angle) do
     angle_cos = :math.cos(angle)
     angle_sin = :math.sin(angle)
     {x * angle_cos + y * angle_sin, x * angle_sin - y * angle_cos}
   end
 
-  @spec distance_to(point2, point2) :: float
+  @spec distance_to(Geo2d.point2, Geo2d.point2) :: float
   def distance_to(v1, v2), do: :math.sqrt(distance_to_squared(v1, v2))
 
-  @spec distance_to_squared(point2, point2) :: float
+  @spec distance_to_squared(Geo2d.point2, Geo2d.point2) :: float
   def distance_to_squared({x1, y1}, {x2, y2}) do
     x = x1 - x2
     y = y1 - y2
--- lib/vector2.ex
+++ lib/vector2.ex
@@ -2,78 +2,78 @@ defmodule Geo2d.Vector2 do
 
   import Geo2d.Constants
 
-  @type vector2 :: {float, float}
-
-  @spec create() :: vector2
+  @spec create() :: Geo2d.vector2
   def create(), do: {0.0, 0.0}
 
-  @spec create(float, float) :: vector2
+  @spec create(Geo2d.point2, Geo2d.point2) :: Geo2d.vector2
+  @spec create(float, float) :: Geo2d.vector2
+  def create({x1, y1}, {x2, y2}), do: {x2 - x1, y2 - y1}
   def create(x, y), do: {x, y}
 
-  @spec create(vector2) :: vector2
-  @spec create([float]) :: vector2
-  @spec create(float) :: vector2
+  @spec create(Geo2d.vector2) :: Geo2d.vector2
+  @spec create([float]) :: Geo2d.vector2
+  @spec create(float) :: Geo2d.vector2
   def create({x, y}), do: {x, y}
   def create(a) when is_float(a), do: {a, a}
   def create([]), do: {0.0, 0.0}
   def create([x, y|_]), do: {x, y}
   def create([x|_]), do: {x, x}
 
-  @spec to_list(vector2) :: [float]
+  @spec to_list(Geo2d.vector2) :: [float]
   def to_list({x, y}), do: [x, y]
 
-  @spec zero() :: vector2
+  @spec zero() :: Geo2d.vector2
   def zero(), do: create()
 
-  @spec identity() :: vector2
+  @spec identity() :: Geo2d.vector2
   def identity(), do: {1.0, 1.0}
 
-  @spec x(vector2) :: float
+  @spec x(Geo2d.vector2) :: float
   def x({x, _y}), do: x
 
-  @spec y(vector2) :: float
+  @spec y(Geo2d.vector2) :: float
   def y({_x, y}), do: y
 
-  @spec xx(vector2) :: vector2
+  @spec xx(Geo2d.vector2) :: Geo2d.vector2
   def xx({x, _y}), do: {x, x}
 
-  @spec yy(vector2) :: vector2
+  @spec yy(Geo2d.vector2) :: Geo2d.vector2
   def yy({_x, y}), do: {y, y}
 
-  @spec xy(vector2) :: vector2
+  @spec xy(Geo2d.vector2) :: Geo2d.vector2
   def xy(v), do: v
 
-  @spec yx(vector2) :: vector2
+  @spec yx(Geo2d.vector2) :: Geo2d.vector2
   def yx({x, y}), do: {y, x}
 
-  @spec at(vector2, integer) :: float
+  @spec at(Geo2d.vector2, integer) :: float
   def at({x, _y}, 0), do: x
   def at({_x, y}, 1), do: y
 
-  @spec magnitude_squared(vector2) :: float
+  @spec magnitude_squared(Geo2d.vector2) :: float
   def magnitude_squared({x, y}), do: x * x + y * y
 
-  @spec magnitude(vector2) :: float
+  @spec magnitude(Geo2d.vector2) :: float
   def magnitude(v), do: :math.sqrt(magnitude_squared(v))
 
-  @spec add(vector2, vector2) :: vector2
+  @spec add(Geo2d.vector2, Geo2d.vector2) :: Geo2d.vector2
   def add({x1, y1}, {x2, y2}), do: {x1 + x2, y1 + y2}
 
-  @spec sub(vector2, vector2) :: vector2
+  @spec sub(Geo2d.vector2, Geo2d.vector2) :: Geo2d.vector2
   def sub({x1, y1}, {x2, y2}), do: {x1 - x2, y1 - y2}
 
-  @spec scale(vector2, float) :: vector2
+  @spec scale(Geo2d.vector2, float) :: Geo2d.vector2
   def scale({x, y}, s), do: {x * s, y * s}
 
-  @spec is_zero(vector2) :: boolean
+  @spec is_zero(Geo2d.vector2) :: boolean
   def is_zero({0.0, 0.0}), do: true
   def is_zero(_v), do: false
 
-  @spec is_identity(vector2) :: boolean
+  @spec is_identity(Geo2d.vector2) :: boolean
   def is_identity({1.0, 1.0}), do: true
   def is_identity(_v), do: false
 
-  @spec is_almost_zero(vector2) :: boolean
+  @spec is_almost_zero(Geo2d.vector2) :: boolean
   def is_almost_zero({x, y}) do
 
     if :math.abs(x) < epsilon and :math.abs(y) < epsilon do
@@ -83,27 +83,27 @@ defmodule Geo2d.Vector2 do
     false
   end
 
-  @spec dot(vector2, vector2) :: float
+  @spec dot(Geo2d.vector2, Geo2d.vector2) :: float
   def dot({x1, y1}, {x2, y2}), do: x1 * x2 + y1 * y2
 
-  @spec cross(vector2, vector2) :: float
+  @spec cross(Geo2d.vector2, Geo2d.vector2) :: float
   def cross({x1, y1}, {x2, y2}), do: x1 * y2 - y1 * x2
 
-  @spec normalize(vector2) :: vector2
+  @spec normalize(Geo2d.vector2) :: Geo2d.vector2
   def normalize({0.0, 0.0}) do
    raise ArithmeticError, message: "Zero vector normalization"
   end
 
-  @spec normalize(vector2) :: vector2
+  @spec normalize(Geo2d.vector2) :: Geo2d.vector2
   def normalize({x, y} = v) do
     inv_len = 1.0 / magnitude(v)
     {x * inv_len, y * inv_len}
   end
 
-  @spec invert(vector2) :: vector2
+  @spec invert(Geo2d.vector2) :: Geo2d.vector2
   def invert({x, y}), do: {-x, -y}
 
-  @spec project(vector2, vector2) :: vector2
+  @spec project(Geo2d.vector2, Geo2d.vector2) :: Geo2d.vector2
   def project({_x, _y}, {0.0, 0.0}) do
     raise ArithmeticError, message: "Projection on zero vector"
   end
