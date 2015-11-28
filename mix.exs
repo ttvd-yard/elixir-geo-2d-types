228a101db45a05f85b8afab42e12b29a41e7990f
Mykola Konyk <mykola@konyk.org>
2015-11-27 23:01:57 -0500

    Adding test, config and mix file.

---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule Geo2dTypes.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :geo_2d_types,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps
+    ]
+  end
+
+  def application do
+    []
+  end
+
+  defp deps do
+    []
+  end
+
+  def package do
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      contributors: ["Mykola Konyk"],
+      licenses: ["MS-RL"],
+      links: %{"GitHub" => "https://github.com/ttvd/elixir-geo-2d-types"}
+    ]
+  end
+end
