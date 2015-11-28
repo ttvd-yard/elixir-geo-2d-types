a5e6e4ba52733df6ad691e90abf774bb6f7a65aa
Mykola Konyk <mykola@konyk.org>
2015-11-28 18:21:46 -0500

    constants: adding pi .

--- lib/constants.ex
+++ lib/constants.ex
@@ -1,6 +1,8 @@
 defmodule Geo2d.Constants do
 
   @spec epsilon :: float
-  def epsilon(), do: 0.000001
+  def epsilon, do: 0.000001
 
+  @spec pi :: float
+  def pi, do: 3.14159265358979323846
 end
