837ae2b8abc64e278a4c3b5ba93013d87b40b726
Mykola Konyk <mykola@konyk.org>
2015-11-28 19:01:39 -0500

    types: some experiments.

--- lib/types.ex
+++ lib/types.ex
@@ -1,5 +1,10 @@
 defmodule Geo2d do
 
+  require Record
+
+  #Record.defrecord :point2, x: 0.0, y: 0.0
+  #@type point2 :: record(:point2, x: float, y: float)
+
   @type point2 :: {float, float}
   @type vector2 :: {float, float}
   @type line2 :: {float, float, float}
