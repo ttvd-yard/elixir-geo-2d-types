53cdb8ce1673d692d003d36927aedb0fd4d3c1b9
Mykola Konyk <mykola@konyk.org>
2015-11-28 21:32:47 -0500

    constants: adding inf, updating epsilon.

--- lib/constants.ex
+++ lib/constants.ex
@@ -1,8 +1,12 @@
 defmodule Geo2d.Constants do
 
   @spec epsilon :: float
-  def epsilon, do: 0.000001
+  def epsilon, do: 1.0e-10
 
   @spec pi :: float
   def pi, do: 3.14159265358979323846
+
+  @spec inf :: float
+  def inf, do: 1.79769e308
+  
 end
