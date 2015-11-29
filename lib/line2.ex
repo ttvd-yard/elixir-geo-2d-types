1a5886c05a51c19f0d0af5564377c11e3581016c
Mykola Konyk <mykola@konyk.org>
2015-11-28 21:10:51 -0500

    line2: adding creation copy function.

--- lib/line2.ex
+++ lib/line2.ex
@@ -3,6 +3,9 @@ defmodule Geo2d.Line2 do
   @spec create() :: Geo2d.line2
   def create(), do: {0.0, 0.0, 0.0}
 
+  @spec create(Geo2d.line2) :: Geo2d.line2
+  def create({a, b, c}), do: {a, b, c}
+
   @spec create(Geo2d.point2, Geo2d.point2) :: Geo2d.line2
   def create({x1, y1}, {x2, y2}), do: {y1 - y2, x2 - x1, x1 * y2 - x2 * y1}
 
@@ -11,5 +14,5 @@ defmodule Geo2d.Line2 do
 
   @spec to_list(Geo2d.line2) :: [float]
   def to_list({a, b, c}), do: [a, b ,c]
-  
+
 end
